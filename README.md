8f89b7562c9d8d56f2cd72bf7dbf558f60b23fb3
DongDong107 <jkds989@gmail.com>
2023-05-03 22:06:22 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DailyNovel_Finale
+
+## 추후 알차게 수정 예정.
---
+++ dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/DailynovelApiApplication.java
@@ -0,0 +1,13 @@
+package com.dailynovel.dailynovelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailynovelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailynovelApiApplication.class, args);
+	}
+
+}
