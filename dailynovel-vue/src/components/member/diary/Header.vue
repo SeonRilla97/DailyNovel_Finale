15d8eb86f842367530e7e32c25e6731da4cd7f2e
SeonRilla <SeonRilla97@gmail.com>
2023-05-09 01:17:22 +0900

    update(diary) : Diary 컴포넌트 분리

---
+++ dailynovel-vue/src/components/member/diary/Header.vue
@@ -0,0 +1,127 @@
+<template>
+    <header class="diary-header">
+        <h1>나의일기</h1>
+            <!-- 드롭다운 메뉴 -->
+            <div class="dropdown">
+                <div class="btn"><span>기분</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div class="btn"><span>날씨</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div class="btn"><span>기분</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div class="btn"><span>솔직함</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div class="btn"><span>날짜</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <div class="btn"><span>날짜</span><span class="icon-clamp"></span></div>
+                <div class="content">
+                    <a href="#">좋음</a>
+                    <a href="#">기쁨</a>
+                    <a href="#">나쁨</a>
+                </div>
+            </div>
+
+            <div class="search-container">
+                <form action="#">
+                <input type="text" placeholder="Search.." name="search">
+                <button>검색</button>
+                </form>
+            </div>
+    </header>
+</template>
+
+<style scoped>
+/* 다이어리 페이지 헤더 */
+.diary-header {
+    display:flex;
+    align-items: center;
+    height:42px;
+    position:relative;
+}
+    .diary-header > h1{
+        margin-left:15px;
+        font-weight:normal;
+        font-size: 20px;
+    }
+    .diary-header .search-container{
+        position:absolute;
+        right: 0;
+        margin: 2%;
+    }
+
+
+/* dropDown(필터 메뉴) */
+.diary-header .dropdown .btn {
+
+font-size: 16px;
+border: none;
+cursor: pointer;
+box-sizing: border-box;
+}
+
+.diary-header .dropdown {
+position: relative;
+display: inline-block;
+margin-left: 40px;
+}
+
+.diary-header .dropdown .content {
+display: none;
+position: absolute;
+background-color: #f9f9f9;
+min-width: 160px;
+box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+z-index: 1;
+}
+
+.diary-header .dropdown .content a {
+color: black;
+padding: 12px 16px;
+text-decoration: none;
+display: block;
+}
+
+.diary-header .dropdown .content a:hover {background-color: #f1f1f1}
+
+.diary-header .dropdown:hover .content{
+display: block;
+}
+.diary-header .dropdown:hover .dropbtn {
+background-color: #3e8e41;
+}
+</style>
