92ca39cb123b25b8bf929063feded07845d36693
SeonRilla <seonrilla97@gmail.com>
2023-05-09 10:02:52 +0900

    modal Layout 삭제, Main에 합침

--- dailynovel-vue/src/router/routes.js
+++ dailynovel-vue/src/router/routes.js
@@ -1,11 +1,8 @@
 import memberLayout from '../components/member/Layout.vue';
-
-import Index from '../components/Index.vue';
-
-import memberModalLayout from '../components/member/modal/Layout.vue';
   import DiaryLayout from '../components/member/diary/Layout.vue'
 
-import memberGuestBookLayout from '../components/member/guestbook/Layout.vue';
+
+import Index from '../components/Index.vue';
 const routes =  [
   { path: '/', component: Index, children:[
       // {path: 'login', component: },
@@ -18,12 +15,8 @@ const routes =  [
   },
 
   { path: '/member', component: memberLayout, children :[
-    {path: 'modal', component: memberModalLayout, children:[
       {path: 'diary', component:DiaryLayout}
     ]} 
-  ]}
-
-
 ]
 
 export default routes;
