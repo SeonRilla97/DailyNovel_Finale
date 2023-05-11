5650ae920f26ef9e5bae5bdeebb7428f2ec25423
DongDong107 <jkds989@gmail.com>
2023-05-11 18:51:26 +0900

    집가기전 푸시

--- dailynovel-vue/src/router/follow.js
+++ dailynovel-vue/src/router/follow.js
@@ -1,5 +1,9 @@
 import Layout from '../components/member/follow/Layout.vue';
+import GuestbookListLayout from '../components/member/guestbook/List.vue';
 
-const route = {path: 'follow', component:Layout ,children:[]};
+const route = {path: 'follow', component:Layout ,children:[
+  {path: 'collection'},
+  {path: 'guestbook', component: GuestbookListLayout }
+]};
 
 export default route;
