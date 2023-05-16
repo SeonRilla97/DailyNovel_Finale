c48bf4a98a40f020818d1c96c158d7bdccc54c2f
DongDong107 <jkds989@gmail.com>
2023-05-16 21:35:15 +0900

    fix(guestbook) : mybatis 변환 작업 시작

---
+++ dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/mbentity/MbGuestBook.java
@@ -0,0 +1,13 @@
+package com.dailynovel.dailynovelapi.mbentity;
+
+import java.sql.Timestamp;
+
+public class MbGuestBook {
+
+  private int id;
+  private int memberId;
+  private int writerId;
+  private String content;
+  private Timestamp regDate;
+
+}
---
+++ dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/mbrepository/MbGuestBookRepository.java
@@ -0,0 +1,5 @@
+package com.dailynovel.dailynovelapi.mbrepository;
+
+public class MbGuestBookRepository {
+
+}
