88f04b9d565e15d04c93f238055e1eeb49d28af9
SeonRilla <seonrilla97@gmail.com>
2023-05-17 11:34:50 +0900

    update(collectiona): 리스트 필터링 백엔드 기능 구현 완료

--- dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/controller/DiaryController.java
+++ dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/controller/DiaryController.java
@@ -1,5 +1,6 @@
 package com.dailynovel.dailynovelapi.controller;
 
+import com.dailynovel.dailynovelapi.mbentity.MbDiaryCollectionView;
 import com.dailynovel.dailynovelapi.service.DiaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,6 +13,8 @@ import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -25,7 +28,8 @@ public class DiaryController {
 
     // 리스트 필터링 (월별로 Diary 불러옴)
     @GetMapping("filter")
-    public String getListGroupingMonthly(
+    public  Map<String,List<MbDiaryCollectionView>> getListGroupingMonthly(
+        @RequestParam(required = true ,defaultValue = "1") int memberId,
         @RequestParam(required = false) String feeling,
         @RequestParam(required = false) String weather,
         @RequestParam(required = false) String date,
@@ -33,23 +37,27 @@ public class DiaryController {
         @RequestParam(required = false) String collection,
         @RequestParam(required = false) String query
     ) throws UnsupportedEncodingException {
-//        Map<String,List<Diary>> list = service.getListGroupingMonthly(
-//            feeling,weather,date,order,collection,query
-//        );
+        
+        //ISO 8601 날짜형식으로 인코딩 된거 Decoder를 통해 LocalDate형식으로 변환
+        LocalDate localDate = null;
+        if(date !=null)
+             localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE_TIME);
+             System.out.println(localDate);
+        Map<String,List<MbDiaryCollectionView>> list = service.getListGroupingMonthly(
+            memberId,feeling,weather,localDate,order,collection,query
+        );
+        System.out.println("어서오세요 컨트롤러 입니다 " +list);
 
-//       ISO 8601 날짜형식으로 인코딩 된거 Decoder를 통해 LocalDate형식으로 변환
-        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE_TIME);
-
-        System.out.println("====================");
-        System.out.println(localDate);
-        System.out.println(collection);
-        System.out.println(feeling);
-        System.out.println(weather);
-        System.out.println(date);
-        System.out.println(query);
-        System.out.println(order);
+        // System.out.println("====================");
+        // System.out.println(localDate);
+        // System.out.println(collection);
+        // System.out.println(feeling);
+        // System.out.println(weather);
+        // System.out.println(date);
+        // System.out.println(query);
+        // System.out.println(order);
 
         
-        return "hi";
+        return list;
     }
 }
