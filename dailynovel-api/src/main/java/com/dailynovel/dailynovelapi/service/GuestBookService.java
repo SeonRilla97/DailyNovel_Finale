0b22321687101fbb003e58d11d934689ead9b3e8
DongDong107 <jkds989@gmail.com>
2023-05-18 22:09:55 +0900

    feat(guestbook) : 마이페이지 구상중

--- dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/service/GuestBookService.java
+++ dailynovel-api/src/main/java/com/dailynovel/dailynovelapi/service/GuestBookService.java
@@ -10,14 +10,22 @@ import com.dailynovel.dailynovelapi.mbentity.MbGuestBookAll;
 public interface GuestBookService {
 
   // 방명록 보기 (주인, 방문자)
-  public List<GuestBook> viewGuestBooks();
+  public List<MbGuestBookAll> viewGuestBookAll();
 
-  // 방명록 답글 쓰기 (주인)
-  public GuestBookComment writeGuestBookComment();
+  public List<MbGuestBookAll> viewGuestBookAll(int id);
 
   // 방명록 쓰기 (방문자)
   public void writeGuestBook(Map<String, Object> guestbook);
 
-  public List<MbGuestBookAll> viewGuestBookAll();
+  // 방명록 수정 (방문자)
+
+  // 방명록 지우기 (주인, 방문자)
+
+  // 방명록 답글 쓰기 (주인)
+  public GuestBookComment writeGuestBookComment();
+
+  // 방명록 답글 수정 (주인)
+
+  // 방명록 답글 삭제 (주인)
 
 }
