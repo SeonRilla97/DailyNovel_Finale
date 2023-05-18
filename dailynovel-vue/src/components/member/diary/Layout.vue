57f1f670d1e20fc13d2c89b11e32963fd26deb12
SeonRilla <seonrilla97@gmail.com>
2023-05-18 09:41:18 +0900

    update(collection) : Layout.vue의 주석처리 구문들 정리

--- dailynovel-vue/src/components/member/diary/Layout.vue
+++ dailynovel-vue/src/components/member/diary/Layout.vue
@@ -17,14 +17,11 @@ const diaryFilter = reactive({
         collection:new Filter(0,["컬렉션","나만의 일기","배부른 일기","더망"]),
         keyword: ""
 })
-getCollectionList();
-getListwithFiltering();
 const diary = reactive({
     list : null
 })
-// onBeforeMount(() => {
-//     getCollectionList();
-// }),
+getCollectionList();
+getListwithFiltering();
 
 // 필터 목록 클릭시 동작 이벤트핸들러
 function filterClickHandler(selected){
@@ -46,8 +43,6 @@ function filterClickHandler(selected){
             break;
         case "date":
             backupMenu.name = "date";
-            console.log("기존에 있던거" + diaryFilter.date);
-            console.log("이 날짜 들어감!" + selected.menuvalue);
             backupMenu.value=diaryFilter.date;
             diaryFilter.date = selected.menuvalue;
             break;
@@ -83,7 +78,8 @@ function getListwithFiltering(backup) {
     const sort = diaryFilter.sort;
     const collection = diaryFilter.collection;
     const keyword = diaryFilter.keyword;
-    // console.log("이거나옴???")
+
+    // 필터링을 위한 쿼리 생성구문
     let query = "?";
     
     if(feeling.idx != 0){
@@ -95,9 +91,6 @@ function getListwithFiltering(backup) {
     if(date!= null){
         let offset = date.getTimezoneOffset() * 60000; 
         let dateOffset = new Date(date.getTime() - offset);
-        // console.log("offset변환전"+date);
-        // console.log("offset변환후"+dateOffset);
-        // console.log("iso8601변환후"+dateOffset.toISOString());
         query += `&date=${dateOffset.toISOString()}`;
     }
     if(sort.idx != 0){
@@ -109,9 +102,8 @@ function getListwithFiltering(backup) {
     if(collection != "" && collection.idx !=0){
         query += `&collection=${collection.id[collection.idx]}`;
     }
+
     // 뽑힌 쿼리에 따라 Fetch 실행
-    // 쿼리를 뽑는 기능과 같이 있는 이유 : 여기 말고 쓰는곳이 따로 없어서
-    //  굳이 함수로 분리할 필요성을 느끼지 못함
     var requestOptions = {
         method: 'GET',
         redirect: 'follow'
@@ -120,6 +112,7 @@ function getListwithFiltering(backup) {
     fetch(`http://localhost:8080/diary/filter${query}`, requestOptions)
         .then(response => response.json())
         .then(result => { 
+            //받아온 데이터가 아무것도 없다면
             if(Object.keys(result).length == 0){
                 prompt("데이터 으으으읍따");
                 
@@ -148,59 +141,51 @@ function getListwithFiltering(backup) {
                         break;
                     }
                 }
-
-
                 return;
             }
+            //받아온 데이터가 있다면
             console.log(result);
-            // 값 넣어주고
+            //diary reactive 객체의 list 속성에 넣고
             diary.list=result
-            // console.log(`객체상태 :${Object.keys(result) }`)
-            // console.log(result)
-            //Date 넘어온것들 Date객체로 생성후 삽입
+            // 모든 데이터의 Date형식을 Javascript의 Date 객체로 변경
             for(let key in diary.list){
-                // console.log(diary.list[key])
                 for(let value in diary.list[key]){
-                    // console.log(diary.list[key][value].sharedEndDate);
-                    // console.log(diary.list[key][value].regDate);
+                    // 공유 만료일이 적힌 데이터라면
                     if(diary.list[key][value].sharedEndDate)
                         diary.list[key][value].sharedEndDate = new Date(diary.list[key][value].sharedEndDate);
+                    // 일기 생성일자
                     diary.list[key][value].regDate = new Date(diary.list[key][value].regDate);
                 }
-                // console.log(diary.list)
-                    // .regDate
-                    // console.log(index);
             }
-            // 데이터가 아무것도 없다면?
             console.log(result);
         })
         .catch(error => console.log('error', error));
     }
-// 컬렉션 메뉴 불러와서 데이터 삽입
+// 컬렉션 데이터 불러와서 데이터 삽입
 function getCollectionList() {
     let requestOptions = {
     method: 'GET',
     redirect: 'follow'
     };
 
-fetch("http://localhost:8080/collection/all", requestOptions)
-  .then(response => response.json())
-  .then(collections => {
-    //배열 생성
-    let tmpnameList = [];
-    let tmpIdList = [];
-    tmpnameList.push("컬렉션")
-    tmpIdList.push(null)
-    for(let index in collections){
-        let col=collections[index];
-        tmpnameList.push(collections[index].name);
-        tmpIdList.push(collections[index].id)
-        // tmpIdList
-    }
-    diaryFilter.collection.menu = tmpnameList;
-    diaryFilter.collection.id = tmpIdList;
-  })
-  .catch(error => console.log('error', error));
+    fetch("http://localhost:8080/collection/all", requestOptions)
+    .then(response => response.json())
+    .then(collections => {
+        //배열 생성
+        let tmpnameList = [];
+        let tmpIdList = [];
+        tmpnameList.push("컬렉션")
+        tmpIdList.push(null)
+        for(let index in collections){
+            let col=collections[index];
+            tmpnameList.push(collections[index].name);
+            tmpIdList.push(collections[index].id)
+            // tmpIdList
+        }
+        diaryFilter.collection.menu = tmpnameList;
+        diaryFilter.collection.id = tmpIdList;
+    })
+    .catch(error => console.log('error', error));
 }
 </script>
 <template>
