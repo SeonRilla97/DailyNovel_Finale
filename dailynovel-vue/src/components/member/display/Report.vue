9cf6da999cd53c46ff955e2f0378ae56af8cb6ea
YooWoo <12hongkildong@gmail.com>
2023-05-23 00:38:50 +0900

    feat(display) 신고하기 기능은 완료, 이제 신고쪽 css꾸며야 함

--- dailynovel-vue/src/components/member/display/Report.vue
+++ dailynovel-vue/src/components/member/display/Report.vue
@@ -1,48 +1,51 @@
 <template lang="">
     <section class="grid">
         <h1 class="d-none">신고페이지</h1>
-        <img src="@/assets/img/display/igotyou.png" alt="img" class="img d-none">
-        <!-- <img src="../../../assets/img" alt=""></img> -->
-        <div>
-            <select name="reason" id="" v-model="reason">
-                <option value="">신고 이유를 선택해주세요.</option>
-                <option value="욕설">욕설</option>
-                <option value="자살암시">자살암시</option>
-                <option value="외설">외설</option>
-                <option value="기타">기타</option>
-            </select>
-            <button @click="transmitReport">전송</button>
-        </div>
-
-        <form class="gform pure-form pure-form-stacked" method="POST" data-email="S2dailynovelS2@gmail.com"
-        action="https://script.google.com/macros/s/AKfycbzJhpwG2sb8vN0okuI8i4DRcCILqthRQQJvf5Dy1spmGBQSxs0q_7CRNzxb83Y1j-6i/exec" target = iframe1>
-            <!-- change the form action to your script url -->
-            <textarea name="message" id="" cols="30" rows="10" placeholder="문의사항을 적어주세요" class="box-large input-boder-none mgt-5 feedback-box-size"></textarea>
-            <div class="d-none">
-                <label for="id">id</label>
-                <input type="text" name="id" placeholder="아이디." maxlength='30' :value="memberId" readonly>
-            </div>
-            <div class="l-box-short mgt-5">
-                <fieldset class="name"><h1>문의종류</h1></fieldset>
-                    <div class="content">
-                        <label for="kind" class="mg-2">버그</label>
-                        <input type="radio" name="kind" id="" value="버그">
-                        <label for="kind" class="mg-2">기능</label>
-                        <input type="radio" name="kind" id="" value="기능">
-                        <label for="kind" class="mg-2">그외</label>
-                        <input type="radio" name="kind" id="" value="그외" checked>
+        <form method="POST" data-email="S2dailynovelS2@gmail.com"
+            action="https://script.google.com/macros/s/AKfycbzJhpwG2sb8vN0okuI8i4DRcCILqthRQQJvf5Dy1spmGBQSxs0q_7CRNzxb83Y1j-6i/exec" 
+            target = iframe1
+            class="grid">
+                <!-- change the form action to your script url -->
+                <section>
+                    <h1 class="d-none">기본 신고 정보</h1>
+                    <div class="d-none">
+                        <label for="신고자Id">MemberId</label>
+                        <input type="text" name="reporterId" placeholder="신고자" maxlength='30' :value="memberId" readonly>
                     </div>
-            </div>
-                <div class="flex-jfy-center mgt-5">
-                    <input class="btn3" type="submit" value="보내기" style="cursor: pointer;">
+                    <div class="d-none">
+                        <label for="신고게시글Id">DiaryId</label>
+                        <input type="text" name="DiaryId" placeholder="일기번호" maxlength='30' :value="id" readonly>
+                    </div>
+                </section>
+                <section>
+                    <h1 class="d-none">신고 세부사항</h1>
+                    <fieldset class="name grid">
+                        <legend>
+                            <h1>신고 이유를 선택해주세요</h1>
+                        </legend>
+                        <select name="reason" id="" v-model="reason">
+                            <option value="">신고 이유를 선택해주세요.</option>
+                            <option value="욕설">욕설</option>
+                            <option value="자살암시">자살암시</option>
+                            <option value="외설">외설</option>
+                            <option value="기타">기타</option>
+                        </select>
+                        <textarea name="message" id="" cols="100" rows="10" placeholder="신고 내용을 입력해주세요." class="resize"></textarea>
+                    </fieldset>
+                </section>
+                <div>
+                    <button @click="goBackHandler">취소하기</button>
+                    <button @click="sendHandler" class="reg">등록하기</button>
                 </div>
+
         </form>
+        <iframe id="iframe1" name="iframe1" class="d-none"></iframe>
     </section>
 
 
 </template>
 <script setup>
-import { ref, defineProps, onUpdated,onMounted, } from 'vue'
+import { ref, defineProps, onUpdated, onMounted, } from 'vue'
 import { useUserDetailsStore } from '../../store/useUserDetailsStore.js'
 
 const props = defineProps({
@@ -51,59 +54,45 @@ const props = defineProps({
     }
 })
 
-onMounted(() => {
-    console.log(memberId);
-}),
+function goBackHandler() {
+    history.back()
+}
+function sendHandler() {
+    alert("신고를 완료했습니다.")
+}
 
-onUpdated(() => {
-    console.log(reason.value);
-})
+// onMounted(() => {
+//      console.log(memberId);
+// }),
+
+// onUpdated(() => {
+//     console.log(reason.value);
+// })
 
 const reason = ref('');
-console.log(reason.value);
+// console.log(reason.value);
 let memberId = useUserDetailsStore().id;
 
-async function transmitReport() {
-    if (reason.value == '')
-        alert('값을 입력하고 전송하세요~')
-    else {
-        try {
-            const response = await fetch(`http://localhost:8080/display/report}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    memberId: memberId,         // 멤버 정보 가지고 오기
-                    diaryId: diaryId,
-                    reason: reason.value,
-                }),
-            });
-
-            if (!response.ok) {
-                throw new Error('요청에 실패했습니다.');
-            }
-
-            const data = response;
-            console.log(data); // 응답 데이터 처리
-        } catch (error) {
-            console.error(error); // 에러 처리
-        }
-
-        setTimeout(load, 50);
-        alert("신고 완료 '~^'")
-    }
-}
 </script>
 <style scoped>
 .grid {
     display: grid;
     justify-items: center;
     align-items: center;
+    grid-auto-flow: row;
 }
 
 .img {
     width: 473.76px;
     height: 442.08px;
+}
+.reg{
+    background-color: #F2C6C2;
+}
+.reg:hover{
+    background-color: #f0a099;
+}
+.resize{
+    resize: none;
 }
 </style>
