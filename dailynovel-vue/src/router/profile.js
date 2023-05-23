efecbee78c833475a448964fa763249de8e5f60a
“Yoo” <12hongkildong@gmail.com>
2023-05-23 10:23:52 +0900

    fix(display) profile.js의 router 중복되던 부분 삭제

--- dailynovel-vue/src/router/profile.js
+++ dailynovel-vue/src/router/profile.js
@@ -1,7 +1,6 @@
 import Layout from '../components/member/profile/Layout.vue';
 import Index from  '../components/member/profile/Index.vue';
 import Follow from '../components/member/profile/Follow.vue';
-import Export from '../components/member/profile/Export.vue';
 import Info from '../components/member/profile/Info.vue';
 import GuestbookListLayout from '../components/member/guestbook/List.vue';
 import Export from  '../components/member/profile/Export.vue';
