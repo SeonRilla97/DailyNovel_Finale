f0f063b565cdf6f0d02eb2c85847a4fffcaa26f1
“Yoo” <12hongkildong@gmail.com>
2023-05-23 10:17:39 +0900

    feat(display) 내보내기 페이지 만들어 보는 중?

---
+++ dailynovel-vue/src/components/member/profile/Export.vue
@@ -0,0 +1,9 @@
+<template lang="">
+    에베베
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    
+</style>
--- dailynovel-vue/src/router/profile.js
+++ dailynovel-vue/src/router/profile.js
@@ -1,11 +1,14 @@
 import Layout from '../components/member/profile/Layout.vue';
 import Index from  '../components/member/profile/Index.vue';
 import GuestbookListLayout from '../components/member/guestbook/List.vue';
+import Export from  '../components/member/profile/Export.vue';
+
 
 const route = {path: 'profile', redirect:"room/profile/index" ,component:Layout ,children:[
   {path: 'index', component:Index},
   {path: 'collection'},
-  {path: 'guestbook', component: GuestbookListLayout }
+  {path: 'guestbook', component: GuestbookListLayout },
+  {path: 'export', component: Export },
 ]};
 
 export default route;
